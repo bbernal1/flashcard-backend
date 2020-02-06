8daaddd610ac9da31dfab70a4deb0ccca718f6e5
Brian <bernaldev3@gmail.com>
2020-02-06 00:29:42 -0500

    Implemented API to update due date of card based on quality value received

--- src/main/java/com/jpcards/backend/repository/CardRepository.java
+++ src/main/java/com/jpcards/backend/repository/CardRepository.java
@@ -1,7 +1,9 @@
 package com.jpcards.backend.repository;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -10,6 +12,9 @@ import com.jpcards.backend.domain.Flashcard;
 
 
 public interface CardRepository extends MongoRepository<Flashcard, String> {
-	List<Flashcard> findAll();
+	
+	//List<Flashcard> findAll();
+	//Optional<Flashcard> findById(String primaryKey);
 	List<Flashcard> findByDueDateLessThanEqual(LocalDate dueDate);
+	//Flashcard save(Flashcard entity);
 }
--- src/main/java/com/jpcards/backend/restservice/FlashcardController.java
+++ src/main/java/com/jpcards/backend/restservice/FlashcardController.java
@@ -3,11 +3,17 @@ package com.jpcards.backend.restservice;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.jpcards.backend.algo.Sm2Impl;
 import com.jpcards.backend.domain.Flashcard;
 import com.jpcards.backend.repository.CardRepository;
 
@@ -24,6 +30,23 @@ public class FlashcardController {
 		Collections.shuffle(cards);
 		return cards;
 	}
+	@CrossOrigin
+	@PutMapping("/flashcards/{quality}")
+	public Flashcard updateDueDate(@RequestBody Flashcard flashcard, @PathVariable int quality) {
+		//get entity
+		Optional<Flashcard> ent = repository.findById(flashcard.getId());
+		if (ent.isPresent()) {
+			Flashcard card = ent.get();
+			Sm2Impl sm2 = new Sm2Impl(card,quality);
+			sm2.calcDueDate();
+			return repository.save(card);
+		}
+		else {
+			return null;
+		}
+		
+	}
+	@CrossOrigin
 	@GetMapping("/getSched")
 	public List<Flashcard> getSched() {
 		List<Flashcard> cards = repository.findByDueDateLessThanEqual(LocalDate.now());
